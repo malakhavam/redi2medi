59c6bd886110636154fac871a3e8957225fed0df
malakhavam <malakhava@yahoo.com>
2022-06-14 22:11:25 -0400

    fixing errors

--- config/connection.js
+++ config/connection.js
@@ -12,7 +12,7 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: process.env.SERVER_PATH,
+      host: 'localhost',
       dialect: 'mysql',
       port: 3306
     }
